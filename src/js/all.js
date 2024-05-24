4525714aa5f957647cc6c4637c21d812bd3fc865
Ozik Jarwo <jarwoozik08@gmail.com>
2024-05-24 19:04:19 +0700

    Implement lazyload image

--- src/js/all.js
+++ src/js/all.js
@@ -1,3 +1,5 @@
+function imageLL(){for(var e=document.getElementsByClassName("lazy"),t=0;t<e.length;t++)isInViewport(e[t])&&(e[t].src=e[t].getAttribute("data-src"))}function isInViewport(e){var t=e.getBoundingClientRect();return t.bottom>=0&&t.right>=0&&t.top<=(window.innerHeight||document.documentElement.clientHeight)&&t.left<=(window.innerWidth||document.documentElement.clientWidth)}function registerListener(e,t){window.addEventListener?window.addEventListener(e,t):window.attachEvent("on"+e,t)};registerListener("load",imageLL),registerListener("scroll",imageLL);
+
 document.addEventListener("DOMContentLoaded", function () {
   document.querySelector(".mobile-nav").addEventListener("click", () => {
     document.querySelector("header nav").classList.toggle("show");
